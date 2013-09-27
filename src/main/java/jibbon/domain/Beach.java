3f258253872d65d58baff6f4b81a603ee452435b
genmymodel <help@genmymodel.com>
2013-09-27 18:53:18 +0200

    commit by genmymodel from www.genmymodel.com: v20130927_185317

---
+++ src/main/java/jibbon/domain/Beach.java
@@ -0,0 +1,131 @@
+package jibbon.domain;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+@javax.persistence.Entity
+public class Beach
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	@javax.persistence.Column(nullable = false)
+	protected String identification;
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	@javax.persistence.Column(nullable = false)
+	protected String measurement;
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	@javax.persistence.Id
+	@javax.persistence.Column(nullable = false)
+	protected final Long id = 0L;
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public Beach(){
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void measure() {
+		// TODO : to implement	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public String getIdentification() {
+		return this.identification;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public String getMeasurement() {
+		return this.measurement;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public long getId() {
+		return this.id;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void setIdentification(String myIdentification) {
+		this.identification = myIdentification;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void setMeasurement(String myMeasurement) {
+		this.measurement = myMeasurement;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void unsetIdentification() {
+		this.identification = "";	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void unsetMeasurement() {
+		this.measurement = "";	
+	}
+	
+}
+
